bf614bdd2ca999b3eb4e6bea6570f1828b09c37b
Vladimir <rimidalv111@live.com>
2019-10-10 22:14:05 -0400

    Assignment 6 submission
    Assignment 6
    submission

---
+++ Assets/Scripts/LoadMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadMenu : MonoBehaviour
+{
+    Vector2 startPos; //grab initial position
+    bool finished = false; //when finished update us
+
+    GameObject startButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position; //grab initial
+        startButton = GameObject.FindGameObjectWithTag("StartButton");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x <= 0) //move the image over to the right until it hits begining of screen
+        {
+            float newLocation = Mathf.Repeat(Time.time * 5f, 20);
+            transform.position = startPos + Vector2.right * newLocation;
+        }
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadWinScene()
+    {
+        //SceneManager.LoadScene(2);
+        int smanager = SceneManager.GetSceneAt(2).buildIndex;
+        SceneManager.LoadScene(smanager);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
